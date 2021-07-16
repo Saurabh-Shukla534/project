04608e161e621c35dd54e8f0b276c1fd6cb54940
Saurabh-Shukla534 <shuklasaurabh534@gmail.com>
2021-07-16 10:01:35 +0530

    fixed date pipe

--- app/src/app/shared/date.pipe.ts
+++ app/src/app/shared/date.pipe.ts
@@ -1,14 +1,13 @@
 import { DatePipe } from "@angular/common";
 import { Pipe, PipeTransform } from "@angular/core";
 
-
 @Pipe({
     name: 'dateformat'
 })
 export class DateFormatPipe implements PipeTransform{
-    transform(date: Date | string, day: number, format: string = 'yyyy-MM-dd') {
+    transform(date: Date | string, format: string = 'dd-MM-yyyy') {
         date = new Date(date);
-        date.setDate(date.getDate()-day);
-        return new DatePipe('en-US').transform(date, format);
+        date.setDate(date.getDate());
+        return new DatePipe('en-IN').transform(date, format);
       }
 }
